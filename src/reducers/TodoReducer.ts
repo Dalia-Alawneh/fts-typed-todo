0db223b08f073892f6d5e29cd2652e0204e45933
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-28 16:16:19 +0300

    Create todoReducer

--- src/reducers/TodoReducer.ts
+++ src/reducers/TodoReducer.ts
@@ -3,7 +3,9 @@ import { TodoItem } from "../types/api";
 type TodoState = {
   todos: TodoItem[]
 }
-type TodoActions = { type: 'UPDATE_TODO'; payload: TodoItem }
+type TodoActions =
+  { type: 'SET_TODOS', payload: TodoItem[] }
+  | { type: 'UPDATE_TODO'; payload: TodoItem }
   | { type: 'DELETE_TODO'; payload: number }
   | { type: 'ADD_TODO'; payload: Omit<TodoItem, 'id'> }
 
@@ -12,8 +14,19 @@ export const todos: TodoState = {
 }
 
 export const todoReducer = (state: TodoState, action: TodoActions) => {
-  switch (action.type){
+  switch (action.type) {
+    case 'SET_TODOS':
+      return { ...state, todos: action.payload };
     case 'UPDATE_TODO':
-
+      return {
+        ...state,
+        todos: state.todos.map(todo => todo.id === action.payload.id ? action.payload : todo)
+      }
+    case 'ADD_TODO':
+      return { ...state, todos: [action.payload, ...state.todos] };
+    case 'DELETE_TODO':
+      return { ...state, todos: [...state.todos.filter(todo => todo.id === action.payload)] }
+    default:
+      return state;
   }
 }
